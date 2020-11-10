e101fe4eb0d0659cd771f46f9b7645c8ff386099
Mohammad Shakeri <programmer.mmdsh@gmail.com>
2020-11-10 22:33:10 +0330

    add image button and rich text box

--- src/pages/AddArticle/AddArticle.jsx
+++ src/pages/AddArticle/AddArticle.jsx
@@ -1,21 +1,65 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import { ArticleService } from "components/Article";
 import { useForm, Controller } from "react-hook-form"
+import JoditEditor from "jodit-react";
+import { storage } from "services/firebase"
+
+const useStyles = makeStyles((theme) => ({
+  input: {
+    display: 'none',
+  },
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "center"
+  }
+}));
 
 export default function AddArticle() {
+  const classes = useStyles();
   const { register, handleSubmit, errors: fieldsErrors, control } = useForm();
+  const [rte, setRte] = React.useState()
+  const onEditorChange = (data) => {
+    setRte(data)
+  }
+
   const onSubmit = (data, e) => {
     e.preventDefault();
-    ArticleService.create(data)
+    const { title, picture } = data;
+    const storageRef = storage().ref();
+    const fileRef = storageRef.child(picture.image[0].name);
+    fileRef.put(picture.image[0]).then(() => {
+      console.log("picture uploaded successfully");
+    })
+    const finalData = {
+      title: title,
+      body: rte,
+    }
+    ArticleService.create(finalData)
       .then(() => alert("Your article successfully added"))
       .catch(error => console.log(error));
   };
 
   return (
     <Grid container spacing={2} style={{ marginTop: 16 }}>
+      <Grid item xs={12} className={classes.buttonContainer}>
+        <input
+          accept="image/*"
+          className={classes.input}
+          id="contained-button-file"
+          type="file"
+          name="picture"
+          ref={register}
+        />
+        <label htmlFor="contained-button-file">
+          <Button variant="contained" color="primary" component="span">
+            Upload Picture
+        </Button>
+        </label>
+      </Grid>
       <Grid item xs={12}>
         <TextField
           inputRef={register({ required: true })}
@@ -28,16 +72,13 @@ export default function AddArticle() {
         />
       </Grid>
       <Grid item xs={12}>
-        <TextField
-          inputRef={register({ required: true })}
-          error={fieldsErrors.body}
-          helperText={fieldsErrors.body ? "body is required" : null}
+        <JoditEditor
           name="body"
-          fullWidth
-          variant="outlined"
-          label="Body"
-          multiline
+          onChange={onEditorChange}
+          inputRef={register}
+          tabIndex={1} // tabIndex of textarea
         />
+
       </Grid>
       <Grid item xs={12}>
         <Button variant="contained" color="primary" onClick={handleSubmit(onSubmit)}>
